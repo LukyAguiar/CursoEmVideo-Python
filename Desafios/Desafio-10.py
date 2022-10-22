afdcd4f528a141cf25334611e6ef751a87bdf23d
Lucas Aguiar <lucas.nune@outlook.com.br>
2022-10-21 22:37:25 -0300

    Dessafio 10 e 11 ajustado e finalizado

--- Desafios/Desafio-10.py
+++ Desafios/Desafio-10.py
@@ -1,3 +1,3 @@
 print("======== DESAFIO 10 ========")
 carteira = float(input("Quanto dinheiro você possui na sua carteira: "))
-print("Caso você queira converter seu dinheiro em dolar, com o valor atual de {} você terá {:.1f} dolares nessa compra.".format(carteira, carteira / 5.16))
+print("Caso você queira converter seu dinheiro em dolar, com o valor atual de {} você terá {:.2f} dolares nessa compra.".format(carteira, carteira / 3.27))
--- Desafios/Desafio-11.py
+++ Desafios/Desafio-11.py
@@ -2,4 +2,6 @@ print("======== DESAFIO 11 ========")
 largura = float(input('Digite a largura da sua parede em metros: '));
 altura = float(input('Digite quanto de altura tem sua parede em metros: '));
 area = largura * altura
-print(area)
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m.'.format(largura, altura, area))
+tinta = area / 2
+print('Para pintar essa parede, você precisará de {}l de tinta.'.format(tinta))
